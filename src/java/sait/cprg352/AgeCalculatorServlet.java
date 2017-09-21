84a759a5cc1a0a7e96bd0e4382fb81500c28cd58
715060 <715060@ELF6IT215926.ACDM.DS.SAIT.CA>
2017-09-21 14:33:30 -0600

    Lab 1 Hello World

---
+++ src/java/sait/cprg352/AgeCalculatorServlet.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sait.cprg352;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 715060
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+          getServletContext().getRequestDispatcher("/WEB-INF/agejsp.jsp").forward(request,response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String message = ""; 
+         
+        try{
+       int ageInput = Integer.parseInt(request.getParameter("ageinput"));
+        ageInput += 1; 
+        message ="Your age next birthday will be: " +ageInput; 
+        } catch(NumberFormatException nfe ){
+             message ="You must give your current age"; 
+        }
+        
+       
+    request.setAttribute("agenext", message); //Use myfirstname in jsp
+        
+       getServletContext().getRequestDispatcher("/WEB-INF/agejsp.jsp").forward(request,response);
+        
+    }
+
+  
+}
---
+++ web/WEB-INF/agejsp.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : agejsp
+    Created on : Sep 21, 2017, 2:06:12 PM
+    Author     : 715060
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age next birthday</h1>
+        <form action ="agecalculator" method = "post">
+           Enter your current age: <input type="text" name = "ageinput">
+            <input type ="submit" value ="Age next birthday">
+            
+        </form>
+        <br>
+        ${agenext}
+    </body>
+</html>
